ae290f9aa2cc9d9c757cfe1511460b663f7c4568
ourhackathon <our.hackathon@gmail.com>
2017-07-14 23:55:13 +0530

    GET

--- src/main/java/com/cts/hackathon/tourist/resources/Attractions.java
+++ src/main/java/com/cts/hackathon/tourist/resources/Attractions.java
@@ -3,7 +3,6 @@ package com.cts.hackathon.tourist.resources;
 import java.util.List;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -37,7 +36,7 @@ public class Attractions {
 		//return "Attractions in Mysore are -- Brindavan Garden, Mysore Palace, Chamundeshwari Temple";
 	}
 
-	@POST
+	@GET
 	@Path("/single")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Attraction getAttraction() {
